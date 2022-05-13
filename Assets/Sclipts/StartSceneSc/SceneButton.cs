f0159551ad3119bd4d8865e57567c1f119437575
Tiger014 <taigadoragon0104@icloud.com>
2022-05-13 19:48:18 +0900

    スタート画面の基礎を作った

---
+++ Assets/Sclipts/StartSceneSc/SceneButton.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;//シーン遷移に使用するクラスの名前空間
+
+public class SceneButton : MonoBehaviour
+{
+	public void OnGameStart()
+	{
+		SceneManager.LoadScene("SampleScene");
+	}
+}
---
+++ Assets/Sclipts/StartSceneSc/TutorialCanvas.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TutorialCanvas : MonoBehaviour
+{
+    public GameObject Canvas;
+    public GameObject TutorialMove;
+    public GameObject Panel;
+    public GameObject Button;
+
+    //ゲーム開始時に実行される
+    void Start()
+    {
+        //パネルを隠す
+        Canvas.SetActive(false);
+    }
+
+    //クリアパネルを表示させる
+    void OnEnter()
+    {
+        //表示されたらムービー以外を消す
+
+        //ムービースキップボタン
+
+        //ムービーが終わったら操作説明を出す
+
+        //操作説明の画面で少し経ったらボタンを表示させる
+    }
+}
