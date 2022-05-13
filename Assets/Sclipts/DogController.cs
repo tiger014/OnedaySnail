ed56154412f2a4fe423a998416b4e2dd301195f4
Tiger014 <taigadoragon0104@icloud.com>
2022-05-13 14:56:21 +0900

    NavMash及びCinemachineのアセット追加 仮ステージ１

---
+++ Assets/Sclipts/DogController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class DogController : MonoBehaviour
+{
+    public Transform goal;
+
+    void Update()
+    {
+        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        agent.destination = goal.position;
+    }
+}
