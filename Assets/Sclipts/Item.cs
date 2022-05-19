54004199cfa500300906e77196d8d190789b94db
Tiger014 <taigadoragon0104@icloud.com>
2022-05-19 17:59:43 +0900

    アイテムを作った

---
+++ Assets/Sclipts/Item.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+	public ParticleSystem particle;
+
+	//衝突時に実行されるメソッド
+	void OnTriggerEnter(Collider other)
+	{
+		// パーティクルを再生する
+		particle.Play();
+
+		// 衝突したら自身を削除する
+		Destroy(gameObject);
+	}
+}
