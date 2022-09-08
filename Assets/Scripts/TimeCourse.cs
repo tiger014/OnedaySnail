b31feb1a978881a6378e3b8ef37fbb34dd3cebbe
DCPC02 <dcpc02@kyoto-seika.ac.jp>
2022-09-08 16:54:32 +0900

    光量変化追加

--- Assets/Scripts/TimeCourse.cs
+++ Assets/Scripts/TimeCourse.cs
@@ -7,7 +7,9 @@ public class TimeCourse : MonoBehaviour
     public bool floowStart;
 
     public GameObject DireLight;
-    float x;
+    float LIntensity;
+    Color LColor;
+
     public float Direspeed;
     float direx;//角度
 
@@ -15,7 +17,9 @@ public class TimeCourse : MonoBehaviour
     public float timefloow;     //時間経過
 
     public float evening;
+    bool onevening;
     public float night;
+    bool onnight;
 
     void Start()
     {
@@ -27,28 +31,23 @@ public class TimeCourse : MonoBehaviour
         evening = timeofday / 2;            //夕方
         night = evening + timeofday / 6;    //夜
 
-        direx = 30f;
-
-        x = 50 / timeofday;
-
+        direx = 30f;                        //始まったときの角度
+        float x = 50 / timeofday;
         Direspeed = x / 60;
 
-
+        LIntensity = DireLight.GetComponent<Light>().intensity;
+        LColor = DireLight.GetComponent<Light>().color;
+        LIntensity = 1.3f;
+        LColor = new Color(1f, 1f, 0.8f);
 
 
     }
     void Update()
     {
-
-
         if (floowStart)
         {
             timefloow += Time.deltaTime;
-
-
             direx += Direspeed;
-
-
         }
         Transform dirTransform = DireLight.transform;   //ディレクショナルライトの角度の取得
         Vector3 worldAngle = dirTransform.eulerAngles;
@@ -56,7 +55,8 @@ public class TimeCourse : MonoBehaviour
         worldAngle.y = 54f;
         worldAngle.z = 0f;
         dirTransform.eulerAngles = worldAngle;
-
+        DireLight.GetComponent<Light>().intensity = LIntensity;    //ライトの光量
+        DireLight.GetComponent<Light>().color = LColor;
 
         if (timefloow >= timeofday)      //一日の終わり
         {
@@ -64,14 +64,25 @@ public class TimeCourse : MonoBehaviour
         }
         else if (timefloow >= night)     //夜
         {
-            Debug.Log("夜");
+            onnight = true;
         }
         else if (timefloow >= evening)    //夕方
         {
-            Debug.Log("夕方");
+            onevening = true;
         }
 
-
-
+        if(onevening)
+        {
+            Debug.Log("夕方");
+            LIntensity -= 0.002f;
+            if(LIntensity <= 0.1f)
+            {
+                LIntensity = 0.1f;
+            }
+        }
+        if (onnight)
+        {
+            Debug.Log("夜");
+        }
     }
 }
