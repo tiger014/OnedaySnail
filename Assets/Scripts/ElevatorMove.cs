7c4abe1b5a7eba0e64d66b24fa501cd77317afd4
DCPC02 <dcpc02@kyoto-seika.ac.jp>
2022-09-26 18:02:57 +0900

    エレベーター作成中

--- Assets/Scripts/ElevatorMove.cs
+++ Assets/Scripts/ElevatorMove.cs
@@ -7,7 +7,12 @@ public class ElevatorMove : MonoBehaviour
     public GameObject Snail;
     public GameObject Kago;
     public GameObject Omori;
+    public GameObject KagoHimo;
+    public GameObject OmoriHimo;
     public float sy;
+    public float _KHz;
+    public float _OHz;
+
     void Update()
     {
         sy = Snail.transform.position.y;
@@ -24,9 +29,9 @@ public class ElevatorMove : MonoBehaviour
 
         Vector3 omoripos = Omori.transform.position;
         omoripos.y = -kagopos.y + 10f;
-        if (omoripos.y >= 5.47f)
+        if (omoripos.y >= 5.2f)
         {
-            omoripos.y = 5.47f;
+            omoripos.y = 5.2f;
         }
 
         Kago.transform.position = kagopos;
